c209246a2e53c2ddcb40df113deb2feddaaa5bce
meklenbpo <yk@kantynent.com>
2021-03-11 15:31:26 +0300

    Added a setup.py installer

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""
+Setuptools installation for YandexGeocoder.
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='YandexGeocoder',
+    version='0.0.1',
+    description='Get point address from X/Y and vice versa',
+    packages=find_packages()
+)
