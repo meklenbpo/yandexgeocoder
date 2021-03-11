8a63a99828f84fb97394ea89034baf0033016e1b
meklenbpo <yk@kantynent.com>
2021-03-11 15:36:58 +0300

    Added two API functions to the __init__.py

---
+++ yandexgeocoder/__init__.py
@@ -0,0 +1,8 @@
+"""
+yandexgeocoder - YandexGeocoder
+
+is a module to access Yandex Geocoder API from Python.
+"""
+
+from .yandexgeocoder import xy_to_address
+from .yandexgeocoder import address_to_xy
