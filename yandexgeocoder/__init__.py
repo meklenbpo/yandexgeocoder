1b3aa0f278f7ffc6cae4b244e47341a42df73112
meklenbpo <yk@kantynent.com>
2021-03-13 15:49:28 +0300

    Added custom errors to the API

--- yandexgeocoder/__init__.py
+++ yandexgeocoder/__init__.py
@@ -6,3 +6,5 @@ is a module to access Yandex Geocoder API from Python.
 
 from .yandexgeocoder import xy_to_address
 from .yandexgeocoder import address_to_xy
+from .yandexgeocoder import ServiceError
+from .yandexgeocoder import UnexpectedResponseError
