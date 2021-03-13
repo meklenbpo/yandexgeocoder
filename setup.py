248806fb69a2da62dc0b9bda698e8a4e7cbfc59c
meklenbpo <yk@kantynent.com>
2021-03-13 15:42:53 +0300

    Version upgrade

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='YandexGeocoder',
-    version='0.0.333',
+    version='0.0.4',
     description='Get point address from X/Y and vice versa',
     packages=find_packages()
 )
